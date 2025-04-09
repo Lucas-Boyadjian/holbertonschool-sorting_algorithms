04479785042053d9a87ee739acdb20c72fe93993
Mylliah <mezhoud.my@gmail.com>
2025-04-09 17:01:23 +0200

    Implement insertion sort algorithm for doubly linked list and add main function for testing

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ *                     order using the Insertion sort algorithm
+ * @list: pointer to the head of the list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			listint_t *prev = temp->prev;
+			listint_t *next = temp->next;
+
+			if (prev->prev != NULL)
+				prev->prev->next = temp;
+			temp->prev = prev->prev;
+
+			if (next != NULL)
+				next->prev = prev;
+			prev->next = next;
+
+			temp->next = prev;
+			prev->prev = temp;
+
+			if (temp->prev == NULL)
+				*list = temp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
